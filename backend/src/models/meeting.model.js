0f9bd7e4448be59ff64349b10c2ce3e9f4f0fea7
DEEPIKASINGH608 <190765168+DEEPIKASINGH608@users.noreply.github.com.>
2026-07-11 07:27:23 +0530

    Add meeting room

---
+++ backend/src/models/meeting.model.js
@@ -0,0 +1,22 @@
+import mongoose, { Schema } from 'mongoose';
+
+const meetingSchema = new Schema({
+    userId: {
+        type: String,
+        required: true,
+    },
+    meetingCode: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+        required: true,
+    },
+})
+
+const Meeting = mongoose.model("Meeting", meetingSchema);
+
+export { Meeting };
+
