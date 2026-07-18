8b1b86f6263acd089adb597cf3350272e0905681
DEEPIKASINGH608 <190765168+DEEPIKASINGH608@users.noreply.github.com.>
2026-07-18 22:10:32 +0530

    Update meeting room

--- frontend/src/pages/NexMeet.jsx
+++ frontend/src/pages/NexMeet.jsx
@@ -126,7 +126,22 @@ export default function NexMeetComponent() {
 
 
   let gotMessageFromServer = (fromId, message) => {
+    var signal = JSON.parse(message)
 
+    if(fromId !== socketIdRef.current) {
+      if(signal.sdp) {
+        connections[fromId].setRemoteDescription(new RTCSessionDescription(signal.sdp)).then(() =>{
+          if(signal.sdp.type === "offer") {
+
+            connections[fromId].createAnswer().then((description) =>{
+              connections[fromId].setLocalDescription(description).then(()=>{
+                socketRef.current.emit("signal", fromId, JSON.stringify({"sdp": connections[fromId].localDescription}))
+              })
+            })
+          }
+        }
+      }
+    }
   }
 
 
