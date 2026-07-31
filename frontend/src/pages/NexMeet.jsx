1122d979525c75403a6818d8b960ff6c548b66f3
DEEPIKASINGH608 <190765168+DEEPIKASINGH608@users.noreply.github.com.>
2026-07-31 14:36:22 +0530

    Improve chat window ui

--- frontend/src/pages/NexMeet.jsx
+++ frontend/src/pages/NexMeet.jsx
@@ -52,7 +52,7 @@ export default function NexMeetComponent() {
         video: {
           width: { ideal: 1280 },
           height: { ideal: 720 },
-          aspectRatio: { ideal: 1.7777777778 } 
+          aspectRatio: { ideal: 1.7777777778 }
         },
         audio: true
       });
@@ -502,4 +502,6 @@ export default function NexMeetComponent() {
     </div>
   );
 }
+
+
 
